13e98ae68c3ad2c1e8dd0e306eaf45542ab82192
Manu <Manu@DESKTOP-07VIKM8>
2016-12-19 19:15:32 +0100

    Deuxième commit : nouveau titre niveau 2 et paragraphe

--- index.php
+++ index.php
@@ -12,6 +12,9 @@ and open the template in the editor.
     <body>
         <h1>Projet GIT ISFCE DvpWeb</h1>
         <p>Vendredi 19 décembre 2016.</p>
+        
+        <h2>Sous-titre</h2>
+        <p>Deuxième modification</p>
         <?php
         // put your code here
         ?>
